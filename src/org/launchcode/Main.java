9dcfff958fd729e6482bb755d1065979cc6123c9
Brendan <mcb0193@comcast.net>
2017-04-03 19:52:17 -0500

    Sort records by population

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -1,6 +1,9 @@
 package org.launchcode;
 
+import org.launchcode.comparators.AreaComparator;
 import org.launchcode.comparators.NameComparator;
+import org.launchcode.comparators.PopulationComparator;
+import org.launchcode.comparators.StateComparator;
 
 import java.util.ArrayList;
 
@@ -10,8 +13,7 @@ public class Main {
 
         ArrayList<City> cities = CityData.loadData();
 
-        // TODO - Use different comparators here
-        NameComparator comparator = new NameComparator();
+        PopulationComparator comparator = new PopulationComparator();
         cities.sort(comparator);
 
         printCities(cities);
